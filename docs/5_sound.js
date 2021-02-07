46d79d8db341f40db6b532a3ba23cfdc0c3cae72
turner <csusbdt@gmail.com>
2021-02-07 07:14:34 -0800

    X

--- docs/5_sound.js
+++ docs/5_sound.js
@@ -1,5 +1,17 @@
 let audio_context = null;
 
+function prepare_audio_context() {
+	if (audio_context === null) {
+		audio_context = new (window.AudioContext || window.webkitAudioContext)();
+	}
+	if (audio_context.state === 'suspended') {
+		audio_context.resume();
+		if (audio_context.state === 'suspended') {
+			throw new Error('suspended');
+		}
+	}
+}
+
 export function c_sound(file) {
 	this.file = file;
 	this.volume = 1;
@@ -11,50 +23,51 @@ export function c_sound(file) {
 
 // fetch allowed before user interaction 
 c_sound.prototype.fetch = function() {
-	if (this.audio_buffer !== null) {
-		return Promise.resolve();
-	}
+	// if (this.audio_buffer !== null) {
+	// 	// no fetch needed
+	// 	return Promise.resolve();
+	// }
 	if (this.array_buffer !== null) {
-		return Promise.reject("already fetched");
+		return Promise.resolve();
 	}
 	return fetch(this.file).then(response => {
 		if (response.ok) {
 			return response.arrayBuffer();
 		} else {
-			throw new Error(`c_sound.fetch<br>${this.file}<br>${response.status}`);
+			throw new Error(`${this.file}<br>${response.status}`);
 		}
 	})
 	.then(array_buffer => {
 		this.array_buffer = array_buffer;
-		return array_buffer;
 	});
 };
 
 // decode requires an audioContext, which requires user interaction 
 c_sound.prototype.decode = function() {
-	return new Promise((resolve, reject) => {
-		if (this.audio_buffer !== null) {
-			return resolve();
-		}
-		if (this.array_buffer === null) {
-			return resolve();
-		}
-		if (audio_context === null) {
-			audio_context = new (window.AudioContext || window.webkitAudioContext)();
-			if (audio_context.state === 'suspended') {
-				return reject("audio_context suspended");
-			}
-		}
-		audio_context.decodeAudioData(
-			this.array_buffer,
-			audio_buffer => {
-				this.array_buffer = null; // can only decode array_buffer once
-				this.audio_buffer = audio_buffer;
-				resolve();
-			},
-			e => reject(e)
-		);
-	});	
+	if (this.audio_buffer !== null) {
+		// decode not needed
+		return Promise.resolve();
+	}
+	const decode_array_buffer = () => {
+		return new Promise((resolve, reject) => {
+			prepare_audio_context();
+			audio_context.decodeAudioData(
+				this.array_buffer,
+				audio_buffer => {
+					this.audio_buffer = audio_buffer;
+					// can only decode array_buffer once
+					this.array_buffer = null; 
+					resolve();
+				},
+				e => reject(e)
+			);
+		});
+	}
+	if (this.array_buffer === null) {
+		return this.fetch().then(decode_array_buffer);
+	} else {
+		return decode_array_buffer();
+	}
 };
 
 // play will call fetch and decode if needed
@@ -63,9 +76,9 @@ c_sound.prototype.play = function(volume) {
 		this.volume = volume;
 	}
 	return Promise.resolve()
-		.then(() => this.fetch())
 		.then(() => this.decode())
 		.then(() => {
+			prepare_audio_context();
 			if (this.buffer_source_node === null) {
 				this.buffer_source_node = audio_context.createBufferSource();
 				this.buffer_source_node.buffer = this.audio_buffer;
@@ -76,6 +89,9 @@ c_sound.prototype.play = function(volume) {
 				this.buffer_source_node.onended = this.stop;
 				this.buffer_source_node.start();
 			}
+		})
+		.catch(() => {
+			this.stop();
 		});
 };
 
@@ -90,6 +106,7 @@ c_sound.prototype.stop = function() {
 	if (this.buffer_source_node !== null) {
 		this.buffer_source_node.onended = null;
 		this.buffer_source_node.stop();
+		this.buffer_source_node.buffer = null;
 		this.buffer_source_node = null;
 		this.gain_node = null;
 	}
