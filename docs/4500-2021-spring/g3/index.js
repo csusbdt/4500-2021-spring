c2d1474e94ee9192c601cd4408780940b2a3549f
turner <csusbdt@gmail.com>
2021-03-15 05:29:09 -0700

    X

--- docs/4500-2021-spring/g3/index.js
+++ docs/4500-2021-spring/g3/index.js
@@ -1,9 +1,8 @@
 import '/4500-2021-spring/scripts/room_main.js';
-//import { c_sound } from '/4500-2021-spring/scripts/audio.js';
-
-//const click = new c_sound('/4500-2021-spring/sfx/click.mp3', 1);
+import { start_room } from '/4500-2021-spring/g3/rooms/start.js';
 
 g.room.start = () => {
 	console.log("g3 started");
-	document.body.style.backgroundColor = g.app.theme_color;
+	document.body.style.backgroundColor = '#FF0000';
+	g.room.current_room = start_room;
 };
--- docs/4500-2021-spring/scripts/room_main.js
+++ docs/4500-2021-spring/scripts/room_main.js
@@ -1,34 +1,39 @@
 import '/4500-2021-spring/scripts/canvas_main.js';
-import { get_audio_context } from '/4500-2021-spring/scripts/audio.js';
-
-let current_room = null;
-
-function get_room(n) {
-	if (current_room === null) {		
-	}
-}
 
 g.room = {
-	start: null
-
+	current_room: null,
+	start: null,
+	touch_point: [g.canvas.d_w / 2, g.canvas.d_h / 2],
+	touch_dirty: false
 };
 
-g.canvas.on_touch = ([x, y]) => {
+g.canvas.on_touch = p => {
+	g.room.touch_point = p;
+	g.room.touch_dirty = true;
 };
 
 g.canvas.update = dt => {
+	if (g.room.current_room && g.room.current_room.update) {
+		g.room.current_room.update(dt);
+	}
+	g.room.touch_dirty = false;
 };
 
 g.canvas.draw_bg = ctx => {
-	g.core.clear_bg();
+	if (g.room.current_room && g.room.current_room.draw_bg) {
+		g.room.current_room.draw_bg(ctx);
+	} else {
+		g.canvas.clear_bg();
+	}
 };
 
 g.canvas.draw_fg = ctx => {
-	ctx.fillStyle = document.body.style.backgroundColor;
-	ctx.fillRect(0, 0, g.canvas.d_w, g.canvas.d_h);
+	g.canvas.clear_fg();
+	if (g.room.current_room && g.room.current_room.draw_fg) {
+		g.room.current_room.draw_fg(ctx);
+	}
 };
 
 g.canvas.start = () => {
-//	document.body.style.backgroundColor = g.app.theme_color;
 	if (g.room.start) g.room.start();
 };
